02c75eb594d11890bec72087ab70f54685386eb3
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-04-27 15:40:17 +0200

    Add API call to download Projects

--- lib/vercel.ts
+++ lib/vercel.ts
@@ -1,12 +1,35 @@
-import { decodeType, nullable, record, string } from "typescript-json-decoder";
+import {
+  array,
+  decodeType,
+  nullable,
+  record,
+  string,
+} from "typescript-json-decoder";
 
+/** Credentials needed to work with the Vercel API */
 export type AccessToken = decodeType<typeof decodeAccessToken>;
+
+/** Decode `AccessToken` from an API response */
 export const decodeAccessToken = record({
   token_type: string,
   access_token: string,
   installation_id: string,
   user_id: string,
   team_id: nullable(string),
+});
+
+/** A Vercel project */
+export type Project = decodeType<typeof decodeProject>;
+
+/**
+ * Decode `Project` from an API response
+ *
+ * TBD: There are many other properties available in the response if someone is interested.
+ */
+export const decodeProject = record({
+  accountId: string,
+  id: string,
+  name: string,
 });
 
 //
@@ -46,3 +69,25 @@ export const getAccessTokenFromClient = async (code: string) =>
   await fetch(`/api/get-access-token?code=${code}`)
     .then((r) => r.json())
     .then(decodeAccessToken);
+
+/** Client-side helper to get all available projects */
+export async function getProjects(
+  accessToken: string,
+  teamId: string | null
+): Promise<Project[]> {
+  const decodeResponse = record({
+    projects: array(decodeProject),
+  });
+  const url = teamId
+    ? `https://api.vercel.com/v4/projects?teamId=${teamId}`
+    : "https://api.vercel.com/v4/projects";
+  const response = fetch(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return await response
+    .then((r) => r.json())
+    .then(decodeResponse)
+    .then((r) => r.projects);
+}
