13ff4614cca0d99963e185dad0f3169f4889e6ae
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-05-03 14:16:13 +0200

    Add API functions to get and create log drains

--- integration/lib/vercel.ts
+++ integration/lib/vercel.ts
@@ -2,8 +2,10 @@ import {
   array,
   decodeType,
   nullable,
+  number,
   record,
   string,
+  union,
 } from "typescript-json-decoder";
 
 /** Credentials needed to work with the Vercel API */
@@ -32,8 +34,27 @@ export const decodeProject = record({
   name: string,
 });
 
+/** A Vercel log drain */
+export type LogDrain = decodeType<typeof decodeLogDrain>;
+
+/** Decode `LogDrain` from an API response */
+export const decodeLogDrain = record({
+  clientId: string,
+  configurationId: string,
+  createdAt: number,
+  id: string,
+  type: union("json", "ndjson", "syslog"),
+  name: string,
+  ownerId: string,
+  projectId: nullable(string),
+  url: string,
+});
+
+/** What format to send logs in, see https://vercel.com/docs/log-drains */
+export type LogDrainType = LogDrain["type"];
+
 //
-// Server-side API Calls
+// API Calls
 //
 
 /**
@@ -60,15 +81,59 @@ export async function getAccessToken(request: {
   return await response.json().then(decodeCredentials);
 }
 
-//
-// Client-side API Calls
-//
-
 /** Client-side helper to trade the temporary setup code for regular access token */
 export const getAccessTokenFromClient = async (code: string) =>
   await fetch(`/api/get-access-token?code=${code}`)
     .then((r) => r.json())
     .then(decodeCredentials);
+
+/** Retrieve a list of all log drains that are defined for the authorized account */
+export async function getLogDrains(
+  accessToken: string,
+  teamId: string | undefined = undefined
+): Promise<LogDrain[]> {
+  const url = getResourceUrl("/v1/integrations/log-drains", teamId);
+  const response = fetch(url, {
+    headers: authHeader(accessToken),
+  });
+  return await response.then((r) => r.json()).then(array(decodeLogDrain));
+}
+
+/** Creates a log drain */
+export async function createLogDrain(
+  accessToken: string,
+  teamId: string | null,
+  drain: {
+    /** The name of the log drain */
+    name: string;
+    /** The type of log format */
+    type: LogDrainType;
+    /**
+     * The url where you will receive logs. The protocol must be `https://` or `http://`
+     * when type is `json` and `ndjson`, and `syslog+tls:` or `syslog:` when the type is
+     * `syslog`.
+     */
+    url: string;
+    /** The project identifier to filter logs */
+    projectId: string | undefined;
+    /**
+     * A secret to sign log drain notification headers so a consumer can verify their
+     * authenticity
+     */
+    secret: string | undefined;
+  }
+): Promise<LogDrain> {
+  const url = getResourceUrl("/v1/integrations/log-drains", teamId);
+  const response = fetch(url, {
+    method: "POST",
+    body: JSON.stringify(drain, null, 2),
+    headers: {
+      "Content-Type": "application/json",
+      ...authHeader(accessToken),
+    },
+  });
+  return await response.then((r) => r.json()).then(decodeLogDrain);
+}
 
 /** Client-side helper to get all available projects */
 export async function getProjects(
@@ -78,16 +143,26 @@ export async function getProjects(
   const decodeResponse = record({
     projects: array(decodeProject),
   });
-  const url = teamId
-    ? `https://api.vercel.com/v4/projects?teamId=${teamId}`
-    : "https://api.vercel.com/v4/projects";
+  const url = getResourceUrl("/v4/projects", teamId);
   const response = fetch(url, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
+    headers: authHeader(accessToken),
   });
   return await response
     .then((r) => r.json())
     .then(decodeResponse)
     .then((r) => r.projects);
+}
+
+//
+// Helpers
+//
+
+const authHeader = (accessToken: string) => ({
+  Authorization: `Bearer ${accessToken}`,
+});
+
+function getResourceUrl(path: string, teamId: string | null = null): string {
+  const base = "https://api.vercel.com";
+  const appendix = teamId ? `?teamId=${teamId}` : "";
+  return `${base}${path}${appendix}`;
 }
